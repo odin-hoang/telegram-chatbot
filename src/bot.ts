321cf99eb8140e252f31bce7e80318be4f16d8bd
odin-hoang <uka.pgr@gmail.com>
2024-10-31 10:51:27 +0700

    Refactor package.json to use nodemon for development

--- src/bot.ts
+++ src/bot.ts
@@ -60,43 +60,84 @@ bot.onText(/\/start/, async (msg) => {
   await saveSession(chatId, { chatId, state: "start" });
 });
 
+bot.on("new_chat_members", async (msg) => {
+  const chatId = msg.chat.id;
+
+  const newMembers = msg.new_chat_members || [];
+  for (const newMember of newMembers) {
+    // Skip if the new member is the bot itself
+    if (
+      newMember.is_bot &&
+      newMember.username === (await bot.getMe()).username
+    ) {
+      continue;
+    }
+    const memberName = newMember.username || newMember.first_name;
+    const message = welcomeMessage.replace("{user}", memberName);
+    bot.sendMessage(chatId, message, {
+      parse_mode: "Markdown",
+    });
+
+    await updateSession(
+      chatId,
+      { userName: memberName, joinedAt: new Date() },
+      memberName
+    );
+  }
+});
+bot.on("left_chat_member", async (msg) => {
+  const chatId = msg.chat.id;
+  const memberName =
+    msg.left_chat_member?.username || msg.left_chat_member?.first_name;
+  if (memberName) {
+    await updateSession(chatId, { leftAt: new Date() }, memberName);
+  }
+});
 bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
   const messageText = msg.text as CommandText;
   const user = msg.from?.username;
 
+  if (user === (await bot.getMe()).username) {
+    return; // Skip messages from the bot itself
+  }
+  if (!messageText) {
+    return; // Skip messages without text
+  }
+
   // Retrieve user session
-  const session = await sessionsCollection.findOne({ chatId });
+  const session = await sessionsCollection.findOne({ chatId, userName: user });
   const currentAddress = session?.address || [];
 
   // Add the new address to the user's address list
-  if (session && session.state === "awaiting_address") {
-    currentAddress.push(messageText);
-    await updateSession(chatId, { address: currentAddress, state: null });
-    bot.sendMessage(chatId, `🗿 Address saved: ${messageText}`);
-    return; // Exit since the address has been handled
-  }
+  // if (session && session.state === "awaiting_address") {
+  //   currentAddress.push(messageText);
+  //   if (user) {
+  //     await updateSession(
+  //       chatId,
+  //       { address: currentAddress, state: null },
+  //       user
+  //     );
+  //   }
+  //   bot.sendMessage(chatId, `🗿 Address saved: ${messageText}`);
+  //   return; // Exit since the address has been handled
+  // }
 
+  // Handle commands
   if (messageText.startsWith("/")) {
     switch (messageText) {
       case CommandText.START:
         const message = welcomeMessage.replace("{user}", user || "User");
-
         bot.sendMessage(chatId, message, {
           parse_mode: "Markdown",
         });
-        await updateSession(chatId, { userName: user, address: [] });
+        await updateSession(
+          chatId,
+          { userName: user, address: [] },
+          user || ""
+        );
         break;
-      case CommandText.WALLET:
-        bot.sendMessage(chatId, "😴 Open Mini App here!", webAppKeyboard);
-        break;
-      case CommandText.HELP:
-        bot.sendMessage(chatId, "ℹ️ Please contact our support team.");
-        break;
-      case CommandText.ADD_ADDRESS:
-        await updateSession(chatId, { state: "awaiting_address" });
-        bot.sendMessage(chatId, "👋 Please enter your address:");
-        break;
+
       case CommandText.GET_ADDRESS:
         const addresses = session?.address || [];
         if (addresses.length === 0) {
@@ -111,7 +152,7 @@ bot.on("message", async (msg) => {
         );
         break;
       case CommandText.FLUSH_ADDRESS:
-        await updateSession(chatId, { address: [] });
+        await updateSession(chatId, { address: [] }, user || "");
         bot.sendMessage(chatId, "🗿 Address list flushed.");
         break;
       default:
@@ -122,7 +163,13 @@ bot.on("message", async (msg) => {
         break;
     }
   } else {
-    bot.sendMessage(chatId, `👋 Nice to meet you, ${user}!`);
+    // Handle normal messages
+    console.log("Normal message:", messageText);
+
+    // Update the number of messages in the session
+    const numberOfMessages = (session?.numberOfMessages || 0) + 1;
+
+    await updateSession(chatId, { numberOfMessages }, user || "");
   }
 });
 
@@ -142,10 +189,14 @@ async function saveSession(chatId: number, sessionData: any) {
 }
 
 // Helper function to update session
-async function updateSession(chatId: number, updates: any) {
+async function updateSession(
+  chatId: number,
+  updates: any,
+  userName: string = ""
+) {
   try {
     await sessionsCollection.updateOne(
-      { chatId },
+      { userName },
       { $set: updates },
       { upsert: true }
     );
@@ -154,11 +205,22 @@ async function updateSession(chatId: number, updates: any) {
     console.error("Error updating session:", err);
   }
 }
+async function getSession(chatId: number, userName: string) {
+  try {
+    const session = await sessionsCollection.findOne({
+      chatId,
+      userName,
+    });
+    return session;
+  } catch (err) {
+    console.error("Error getting session:", err);
+  }
+}
 
-cron.schedule("*/1 * * * *", async () => {
-  console.log("Running cron job...");
-  triggerSendNotify();
-});
+// cron.schedule("*/1 * * * *", async () => {
+//   console.log("Running cron job...");
+//   triggerSendNotify();
+// });
 
 const inlineKeyboard = {
   inline_keyboard: [
@@ -176,28 +238,28 @@ const inlineKeyboard = {
   resize_keyboard: true,
   one_time_keyboard: true,
 };
-async function triggerSendNotify() {
-  try {
-    const users = await sessionsCollection.find().toArray();
-    for (const user of users) {
-      const chatId = user.chatId;
-      const addresses = user.address || [];
-      const DECIMAL = Math.pow(10, 8);
-      for (const address of addresses) {
-        const transactions = await getTransaction(address, "0");
-        transactions.forEach((tx: any) => {
-          const message = transactionMessage
-            .replace("{txHash}", tx.transaction_hash)
-            .replace("{amount}", `${tx.amount / DECIMAL}`)
-            .replace("{timestamp}", `${new Date(tx.timestamp / 1000)}`);
-          bot.sendMessage(chatId, message, {
-            parse_mode: "Markdown",
-            reply_markup: inlineKeyboard,
-          });
-        });
-      }
-    }
-  } catch (err) {
-    console.error("Error in triggerSendNotify:", err);
-  }
-}
+// async function triggerSendNotify() {
+//   try {
+//     const users = await sessionsCollection.find().toArray();
+//     for (const user of users) {
+//       const chatId = user.chatId;
+//       const addresses = user.address || [];
+//       const DECIMAL = Math.pow(10, 8);
+//       for (const address of addresses) {
+//         const transactions = await getTransaction(address, "0");
+//         transactions.forEach((tx: any) => {
+//           const message = transactionMessage
+//             .replace("{txHash}", tx.transaction_hash)
+//             .replace("{amount}", `${tx.amount / DECIMAL}`)
+//             .replace("{timestamp}", `${new Date(tx.timestamp / 1000)}`);
+//           bot.sendMessage(chatId, message, {
+//             parse_mode: "Markdown",
+//             reply_markup: inlineKeyboard,
+//           });
+//         });
+//       }
+//     }
+//   } catch (err) {
+//     console.error("Error in triggerSendNotify:", err);
+//   }
+// }
